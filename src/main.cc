087717d4840d982d234fa2fcc8bb7b25cf0e2bfc
Ritsav <shrestharitsav@gmail.com>
2025-02-23 22:17:51 +0545

    Initial Commit Fix: HashMap Implementation & Book Class

--- src/main.cc
+++ src/main.cc
@@ -1,25 +1,7 @@
 #include "hashmap.h"
 #include "book.h"
-#include "linked_list.h"
 #include<memory>
 #include<vector>
 
 int main(){
-    // Test Run ----- Temporary
-    auto person = std::make_shared<LinkedList>("Bunny");
-    LinkedList::start_ = person;
-
-    // List of all the names to insert
-    std::vector<std::string> names{"Usagi", "Neko", "Kimiko"};
-    
-    // Insert all the names using the same variable
-    for(const auto& name: names){
-        auto person = std::make_shared<LinkedList>(name);
-        insert(person);
-    }
-
-    auto map = std::make_unique<HashMap<LinkedList>>();
-    map->insert(LinkedList::start_);
-
-    map->listAllBucketObjects(1);
 }
